0f8ba33aa80ddfdaec8e8ccdced85f8b5fce17a9
Grimnack <matthieu.caron@laposte.net>
2015-02-17 16:50:25 +0100

    Ca compilera pas faut commenter dotise

--- arbre.hs
+++ arbre.hs
@@ -4,11 +4,11 @@ import Test.QuickCheck
 
 data Arbre coul val = Vide | Noeud coul val (Arbre coul val) (Arbre coul val) deriving Show
 
-mapArbre _ Vide = Vide
+mapArbre _  Vide                         = Vide
 mapArbre f (Noeud coul val gauche droit) = Noeud (fst (f coul val)) (snd (f coul val)) (mapArbre f gauche) (mapArbre f droit)
 
 foldArbre :: (couleur -> a -> b -> b -> b) -> b -> Arbre couleur a -> b
-foldArbre _ v Vide = v
+foldArbre _ v  Vide                         = v
 foldArbre f v (Noeud coul val gauche droit) = f coul val (foldArbre f v gauche) (foldArbre f v droit) 
 
 --Petit Arbre de test 
@@ -17,11 +17,11 @@ test'= Noeud "rouge" 1 (Noeud "rouge" 2 Vide Vide) (Noeud "rouge" 3 Vide Vide)
 
 
 --Version non terminale de taille 
-taille Vide = 0
+taille  Vide                    = 0
 taille (Noeud _ _ gauche droit) = 1 + taille gauche + taille droit
 	 
 --Version non terminale de hauteur
-hauteur Vide = 0
+hauteur  Vide                    = 0
 hauteur (Noeud _ _ gauche droit) = max ((hauteur gauche)+1,(hauteur droit)+1)
 										where max (x,y) = if x < y then y else x
 										
@@ -31,7 +31,7 @@ hauteur (Noeud _ _ gauche droit) = max ((hauteur gauche)+1,(hauteur droit)+1)
 
 
 peigneGauche :: [(c,a)] -> Arbre c a
-peigneGauche [] = Vide
+peigneGauche []              = Vide
 peigneGauche ((coul,val):xs) = Noeud coul val (peigneGauche xs) Vide
 
 --Question 5
@@ -43,7 +43,7 @@ prop_hauteurPeign xs = length xs == hauteur (peigneGauche xs)
 
 --Question 7
 estComplet :: Arbre c a -> Bool
-estComplet Vide = True
+estComplet  Vide                    = True
 estComplet (Noeud _ _ gauche droit) = if taille gauche == taille droit then
 								      	(estComplet gauche) && 
 									  	(estComplet droit)   
@@ -87,7 +87,25 @@ element a (Noeud _ b gauche droite)= if a == b then
                                      else
                                        (element a gauche) || (element a droite)
 
+--Question 14
+noeud :: (c -> String) -> (a -> String) -> (c,a) -> String
+noeud fcol fval (c,v) = fval v ++ fcol c 
 
+--Question 15
+arcs :: Arbre c a -> [(a,a)]
+arcs  Vide                     = []
+arcs (Noeud _ a Vide Vide)     = []
+arcs (Noeud _ a gauche Vide)   = [(a,b)] ++ arcs gauche 
+                                 where (Noeud _ b _ _ ) = gauche
+arcs (Noeud _ a Vide droite)   = [(a,b)] ++ arcs droite 
+                                 where (Noeud _ b _ _ ) = droite
+arcs (Noeud _ a gauche droite) = [(a,b)] ++ [(a,c)] ++ arcs gauche 
+                                 where (Noeud _ b _ _ ) = gauche
+                                       (Noeud _ c _ _ ) = droite
+                                     
+                                     
+arc :: (a -> String) -> (a,a) -> String
+arc farc (a,b) = farc a ++ " -> " ++ farc b ++ "\n"
 
 
 
